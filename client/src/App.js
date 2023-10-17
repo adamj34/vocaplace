8514427666cded71aae8f88a909567482a3bc016
oworobjew <oworobjew@gmail.com>
2023-10-17 15:52:58 +0200

    react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          tu bedzie duolingo
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
