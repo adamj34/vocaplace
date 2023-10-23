39a54e56f7b83c84f5b7d0b701958d61d8d2b0a4
root <root@DESKTOP-PQ2EOLP>
2023-10-23 14:56:13 +0200

    configured server for local development and added db connection

--- server/server.js
+++ server/server.js
@@ -1,16 +1,38 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
-const PORT = process.env.PORT || 3000;
+import db from "./db.js";
 
 const app = express();
+
+await db.testConnection();
+const dbConnection = await db.getConection();
+
+// try {
+//     const createTableQuery = `
+//         CREATE TABLE IF NOT EXISTS example_table (
+//             id SERIAL PRIMARY KEY,
+//             name VARCHAR(100),
+//             email VARCHAR(100)
+//         )
+//     `;
+
+//     await dbConnection.none(createTableQuery);
+//     console.log("Table created successfully");
+// } catch (error) {
+//     console.error("Error creating table: ", error);
+// }
+
 
 app.use(express.json( {limit: '10mb'} ));
 app.use(cors());
 
 
+app.get("/", (req, res) => {
+    res.send("Hello World!!!");
+});
+
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
