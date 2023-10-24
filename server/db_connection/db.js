001561693d0d3480fed4ea2a9a3131d5d2c8acb5
addar <trashci18@gmail.com>
2023-10-24 14:48:21 +0200

    test branch push

--- server/db_connection/db.js
+++ server/db_connection/db.js
@@ -4,6 +4,6 @@ import dbConfig from './db-config.js';
 
 const pgp = pgPromise({});
 
-const db = pgp(dbConfig);
+const db = pgp(dbConfig);  // db - singleton instance of the database connection
 
 export default db;
