97bb2c398106c6075dbcd89448bd131bbd9f614e
KrystianBetka <k.betka.718@studms.ug.edu.pl>
2023-11-15 19:34:24 +0100

    keycloak dla serwera( chyba dziala )

--- server/server.js
+++ server/server.js
@@ -1,14 +1,37 @@
 import express from "express";
 import cors from "cors";
-import db  from "./db/connection/db.js";
+import db from "./db/connection/db.js";
 import testConnection from "./db/connection/testConnection.js";
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
 
 const app = express();
- 
+
 await testConnection(db);
 
-app.use(express.json( {limit: '10mb'} ));
+const memoryStore = new session.MemoryStore();
+const keycloak = new Keycloak({ store: memoryStore }, './keycloak.json'); // Specify the path to your keycloak.json file
+
+// Session
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+
+app.use(keycloak.middleware());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
+
+app.get('/test', keycloak.protect(), function (req, res) {
+    res.render(
+        'test',
+        { title: 'Testing keycloak on the server' }
+    );
+});
+
+app.use(keycloak.middleware({ logout: '/' }));
 
 app.get("/", (_req, res) => {
     db.questions.create()
@@ -16,16 +39,15 @@ app.get("/", (_req, res) => {
             res.json({
                 'success': true,
                 'message': 'Questions table created successfully'
-            })
+            });
         })
         .catch(err => {
             res.json({
                 'success': false,
                 'message': err.message
-            })
-        })
+            });
+        });
 });
-  
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
