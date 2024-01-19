edcfaa2d48d3ae548e34290c39f907fa09c226ad
addar <trashci18@gmail.com>
2024-01-19 23:20:58 +0100

    minor change

--- server/api/getUserDataMiddleware.js
+++ server/api/getUserDataMiddleware.js
@@ -2,13 +2,13 @@ import { decode } from 'jsonwebtoken';
 
 // Middleware to extract user ID and preffered_username from token
 function getUserData(req, _res, next) {
-    // const token = req.headers.authorization.split(' ')[1]; // Extract the token from the Authorization header
-    // const decodedToken = decode(token); // Decode the token
-    // req.userId = decodedToken.sub; // Get the user ID and add it to the request object
-    // req.username = decodedToken.preferred_username;
+    const token = req.headers.authorization.split(' ')[1]; // Extract the token from the Authorization header
+    const decodedToken = decode(token); // Decode the token
+    req.userId = decodedToken.sub; // Get the user ID and add it to the request object
+    req.username = decodedToken.preferred_username;
 
-    req.userId = '8a697eb1-786d-4b9d-8df8-dd891901284d'
-    req.username = 'x'
+    // req.userId = '8a697eb1-786d-4b9d-8df8-dd891901284d'
+    // req.username = 'x'
     next(); 
 }
 
