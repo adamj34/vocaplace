bf213a3d42566e8d8bc37f52d0e9922b249d247b
addar <trashci18@gmail.com>
2024-06-08 20:51:35 +0200

    added PKCE to keyclok, server and client

--- client/src/Keycloak.js
+++ client/src/Keycloak.js
@@ -8,6 +8,7 @@ const keycloak = new Keycloak({
  url: `${window.location.protocol}//${window.location.hostname}:${keycloak_port}`,
  realm: keycloak_realm,
  clientId: keycloak_client_id,
+ pkceMethod: 'S256',
 });
 
 export default keycloak;
