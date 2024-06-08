a72f116a998c8cb84143956b631cbb727c19f033
addar <trashci18@gmail.com>
2024-06-08 17:30:12 +0200

    removed utils copy

--- server/src/validation/utils/multerUpload.js
+++
@@ -1,7 +0,0 @@
-import multer from "multer";
-
-// keep the uploaded file in memory without saving it to the disk
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-export default upload;
